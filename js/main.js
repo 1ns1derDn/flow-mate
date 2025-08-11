af378b5bb72c450e4fc9449f89a880826eff13d3
1ns1derDn <serhiienko98@gmail.com>
2025-08-11 20:02:36 +0400

    fixed

--- js/main.js
+++ js/main.js
@@ -36,4 +36,16 @@
 			})
 		})
 	}
+
+	const header = document.querySelector(".header-js")
+
+	if (header) {
+		window.addEventListener("scroll", () => {
+			if (window.scrollY > 50) {
+				header.classList.add("sticky")
+			} else {
+				header.classList.remove("sticky")
+			}
+		})
+	}
 })()
